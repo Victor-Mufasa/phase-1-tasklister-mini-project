de33238df9ccf8da9adacada97611b2b47d26e49
Victor Kemboi <rickvick142@gmail.com>
2026-07-31 22:49:24 +0300

    Finished Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+  form.addEventListener("click", (event) => {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value.trim();
+    if (taskDescription !== "") {
+      // Creating list item
+      const li = document.createElement("li");
+      li.textContent = taskDescription;
+      taskList.appendChild(li);
+      taskInput.value = "";
+    }
+  });
 });
