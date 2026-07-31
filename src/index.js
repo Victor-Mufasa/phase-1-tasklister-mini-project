ba961bf7fadd6dd4aef3a211f91495f32f184e41
Victor Kemboi <rickvick142@gmail.com>
2026-07-31 23:22:35 +0300

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.querySelector("#create-task-form");
   const taskList = document.querySelector("#tasks");
-  form.addEventListener("click", (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
     const taskInput = document.getElementById("new-task-description");
     const taskDescription = taskInput.value.trim();
@@ -10,6 +10,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
       // Creating list item
       const li = document.createElement("li");
       li.textContent = taskDescription;
+
+      // Add delete button
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "✕";
+      deleteBtn.style.marginLeft = "10px";
+      deleteBtn.style.color = "red";
+      deleteBtn.style.cursor = "pointer";
+      deleteBtn.style.border = "none";
+      deleteBtn.style.background = "none";
+      // Adding an event listener to the delete button
+      deleteBtn.addEventListener("click", function () {
+        li.remove();
+      });
+
+      li.appendChild(deleteBtn);
       taskList.appendChild(li);
       taskInput.value = "";
     }
